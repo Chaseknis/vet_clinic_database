aba5f28eb0f228d633285c41499e7bb54972d63d
chaseknis <chase6th@gmail.com>
2023-03-30 17:04:43 +0200

    update the animals table Create a savepoint for the transaction Rollback to the savepoint Commit transaction

--- queries.sql
+++ queries.sql
@@ -8,3 +8,73 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+
+SELECT * FROM public.animals
+ORDER BY id ASC 
+
+BEGIN;
+DELETE FROM animals where id=0;
+COMMIT;
+
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+
+	VALUES ('Ditto', '2022-05-14', 4, true, 22, '');
+	
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+SELECT * FROM animals;
+
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT * FROM animals;
+
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+SELECT * FROM animals;
+
+ROLLBACK;
+SELECT * FROM public.animals
+ORDER BY id ASC
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SELECT * FROM animals;
+
+SAVEPOINT my_savepoint;
+
+UPDATE animals SET weight_kg = -1 * weight_kg;
+SELECT * FROM animals;
+
+ROLLBACK TO SAVEPOINT my_savepoint;
+
+UPDATE animals SET weight_kg = -1 * weight_kg WHERE weight_kg < 0;
+SELECT * FROM animals;
+
+COMMIT;
+
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, SUM(escape_attempts) as total_escapes 
+FROM animals 
+GROUP BY neutered 
+ORDER BY total_escapes DESC 
+LIMIT 1;
+
+SELECT species, MIN(weight_kg) as min_weight, MAX(weight_kg) as max_weight 
+FROM animals 
+GROUP BY species;
+
+SELECT species, AVG(escape_attempts) as avg_escapes 
+FROM animals 
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' 
+GROUP BY species;
--- schema.sql
+++ schema.sql
@@ -1,11 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     name character varying(250) COLLATE pg_catalog."default" NOT NULL,
     date_of_birth date,
     escape_attempts integer,
     neutered boolean,
     weight_kg numeric,
+    species character(1) COLLATE pg_catalog."default",
     CONSTRAINT animals_pkey PRIMARY KEY (id)
 );
